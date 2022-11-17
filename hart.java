583e2dd432fe79b0dcbb73658d097a4daa57bf43
mTy8421 <koontyza007@gmail.com>
2022-11-17 09:02:38 +0700

    update hart.java

---
+++ hart.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+/**
+ * index
+ */
+public class index {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int size = scan.nextInt();
+
+        hart(size);
+
+    }
+
+    private static void hart(int size) {
+        int alpha = 65;
+
+        for (int i = size / 2; i < size; i += 2) {
+
+            for (int j = 1; j < size - i; j += 2) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j < i + 1; j++) {
+                System.out.print((char) (alpha + j - 1));
+            }
+
+            for (int j = 1; j < size - i + 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j < i + 1; j++) {
+                System.out.print((char) (alpha + j - 1));
+            }
+            System.out.println();
+        }
+
+        for (int i = size; i > 0; i--) {
+
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j < i * 2; j++) {
+                System.out.print((char) (alpha + j - 1));
+
+            }
+
+            System.out.println();
+        }
+
+    }
+}
